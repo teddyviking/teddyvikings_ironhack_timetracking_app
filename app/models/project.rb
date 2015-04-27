8049e445b4e004522b27c00e93626d33b1aa2c7f
Rafael Tejado <rtejado90@gmail.com>
2015-04-27 17:06:38 +0200

    Add seed

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,10 @@ class Project < ActiveRecord::Base
 	def self.iron_find(id)
 		self.where("id =?", id).first
 	end
+
+	def self.clean_old
+		projects = self.where("created_at < ?", 1.week.ago)
+		projects.destroy_all
+	end
+	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.all.destroy_all
+projects = Project.create([{ id: 1, name: 'Iron F', description: 'Cool project'},
+							{id: 2, name: 'lovely day', description: "Not any more"},
+							{id: 3, name: 'Final Ironhack project', description: 'You are gonna die, baby'}])
